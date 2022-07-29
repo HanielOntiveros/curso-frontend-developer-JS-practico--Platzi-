443ac1b73027a5a8d8bc6481c869c6f027006b04
Haniel Ontiveros <haniel97@hotmail.com>
2022-07-28 21:57:27 -0600

    Toogle mobile menu

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menu = document.querySelector('.menu')
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 
+menu.addEventListener('click',toggleMobileMenu);
+
 function toggleDesktopMenu(){
-    console.log('hi')
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
